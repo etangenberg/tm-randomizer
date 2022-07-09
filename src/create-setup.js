4b3fb88c83bee9053f0eb57e9a367a9015862932
Edwin Tangenberg <edwin.tangenberg@poldervalley.com>
2022-07-09 16:45:25 +0200

    applied validation
    Signed-off-by: Edwin Tangenberg <edwin.tangenberg@poldervalley.com>

--- src/create-setup.js
+++ src/create-setup.js
@@ -1,8 +1,16 @@
 import generateTiles from './round-tiles/generate-round-tiles';
 import generateCards from './bonus-cards/generate-bonus-cards';
 
+import scoring from './data/round-tiles.json';
+
 const createSetup = (playerCount) => {
-  const roundTiles = generateTiles(6);
+  const validRoundTile = (round, key) => {
+    if (round < 5) return true;
+
+    const tile = scoring.find((t) => t.key === key);
+    return !tile.id.startsWith('Spade'); 
+  }
+  const roundTiles = generateTiles({ validRoundTile });
   const bonusCards = generateCards(playerCount);
   return `${roundTiles.join('-')}_${bonusCards.join('-')}`;
 };

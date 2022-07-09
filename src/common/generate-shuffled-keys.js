2fe37225637cef125c0eae053bcee1a95736abad
Edwin Tangenberg <edwin.tangenberg@poldervalley.com>
2022-07-09 16:35:31 +0200

    possible to inject config
    Signed-off-by: Edwin Tangenberg <edwin.tangenberg@poldervalley.com>

--- src/common/generate-shuffled-keys.js
+++ src/common/generate-shuffled-keys.js
@@ -1,12 +1,26 @@
 import createArrray from "./create-array";
-import shuffleArray from "./shuffle-array";
+import shuffleArrayDefault from "./shuffle-array";
 
-const generateShuffledKeys = (keep, total) => {
+const generateShuffledKeys = (keep, total, config = {}) => {
+  const { 
+    validRoundTile: valid,
+    shuffleArray,
+  } = config;
   if (keep > total) throw new Error('total cannot be less than keep');
 
   const keys = createArrray(total);
-  const shuffledKeys = shuffleArray(keys);
-  return shuffledKeys.slice(0, keep);
+  const shuffledKeys = shuffleArray ? shuffleArray(keys) : shuffleArrayDefault(keys);
+  
+  const keeps = [];
+
+  while (keeps.length < keep) {
+    const current = shuffledKeys.splice(0,1)[0];
+    if (valid === undefined || valid(keeps.length + 1, current)) {
+      keeps.push(current);
+    }
+  }
+  
+  return keeps;
 };
 
 export default generateShuffledKeys;
